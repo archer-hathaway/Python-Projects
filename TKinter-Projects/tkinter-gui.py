cad979271e38a8c1600e2ee40d451c01e9fe93d4
mike daniels <micodian83@gmail.com>
2020-06-11 12:16:17 -0500

    added requests module

--- TKinter-Projects/tkinter-gui.py
+++ TKinter-Projects/tkinter-gui.py
@@ -1,9 +1,16 @@
 #import tkinter module
 import tkinter as tk
+import requests as requests
 
 
 #define function
 #function displays text in the root window
+def iss_location():    
+    iss_location_now = requests.get('http://api.open-notify.org/iss-now.json').text
+    #location_string = print(iss_location) 
+    tk.Label(root, text=iss_location_now).pack()
+   
+
 def red_apple():
     tk.Label(root,text="Always looks red").pack()
     
@@ -27,7 +34,7 @@ root.title("My Program")
 #myLabel2.grid(row=1,column=0)
 #create button
 #button for red apple
-tk.Button(frame, text="RED APPLE", fg= "red", command= red_apple).pack(side= tk.LEFT)
+tk.Button(frame, text="RED APPLE", fg= "red", command= iss_location).pack(side= tk.LEFT)
 #redButton.bind("<Button-1>",red_apple)
 #button for green apples
 tk.Button(frame, text="Green Apple", fg="green", command = green_apple).pack(side=tk.LEFT)
