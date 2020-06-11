ab0206908d22121e237a28bdae05acc7dfd1c985
mike daniels <micodian83@gmail.com>
2020-06-10 20:19:13 -0500

    added binding button to display text

--- tkinter-gui.py
+++ tkinter-gui.py
@@ -3,15 +3,20 @@ import tkinter as tk
 
 
 #define function
+#function displays text in the root window
 def red_apple():
-    print('Always looks red')
+    tk.Label(root,text="Always looks red").pack()
+    
 def green_apple():
-    print('Always looks green')
+    tk.Label(root,text= "Always looks green").pack()
 
-#root widget
+#root widget window
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
+
+#title on root window
+root.title("My Program")
 #creating a label widget
 #define and put up on the screen
 #myLabel = tk.Label(root, text ="Hello World!")
@@ -21,9 +26,11 @@ frame.pack()
 #myLabel.grid(row=0,column=0)
 #myLabel2.grid(row=1,column=0)
 #create button
-redButton = tk.Button(frame, text="RED APPLE", fg= "red", command= red_apple)
-redButton.pack(side= tk.LEFT)
-greenButton = tk.Button(frame, text="Green Apple", fg="green", command = green_apple)
-greenButton.pack(side= tk.LEFT)
+#button for red apple
+tk.Button(frame, text="RED APPLE", fg= "red", command= red_apple).pack(side= tk.LEFT)
+#redButton.bind("<Button-1>",red_apple)
+#button for green apples
+tk.Button(frame, text="Green Apple", fg="green", command = green_apple).pack(side=tk.LEFT)
+
 #main Loop
 root.mainloop()
