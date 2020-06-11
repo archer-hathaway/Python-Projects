72a98237028db0dec3d07aae69b35599f703e63f
mike daniels <micodian83@gmail.com>
2020-06-11 13:21:31 -0500

    updated json module

--- API-PROJECTS/nasaApiTest.py
+++ API-PROJECTS/nasaApiTest.py
@@ -2,13 +2,27 @@
 # Using open notify API
 #
 #import requests module
-import requests as requests
+import requests
+import json
 
 #create request object to get() which holds the url 
-request = requests.get('http://api.open-notify.org')
+#request = requests.get('http://api.open-notify.org')
 #Get html as a content
 #print(request.text)
 #print(request.status_code)
 #using the endpoint 
-iss_location = requests.get('http://api.open-notify.org/iss-now.json').text
-print(iss_location)
+#get location of iss
+#iss_location = requests.get('http://api.open-notify.org/iss-now.json').text
+#print(iss_location)
+#get number of people in space
+iss_people = requests.get('http://api.open-notify.org/astros.json')
+data = iss_people.json()
+print(data)
+print(data['number'])
+#
+"""
+parameter = {"lat": 37.78, "lon": -122.41}
+response = requests.get("http://api.open-notify.org/iss-pass.json",params=parameter)
+data =response.json()
+print(data)
+"""
