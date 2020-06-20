0acf981f78150a93d8432d0082a818f5c78da91d
mike daniels <micodian83@gmail.com>
2020-06-20 11:55:46 -0500

    Update string.py

--- string.py
+++ string.py
@@ -1,25 +1,26 @@
-"""
-try:
-    strings = input()
-    string_count = []
-    for i in strings:        
-        if i == i.upper():
-             string_count.append(i)
-    print(len(string_count))
-except ValueError:    
-    print('confused')
-"""
+#
+#
+#
+#
 
-
-"""
+#function to return count of UpperCase letters in a given string
 def upperCase_count(string):
-    string_count=[]
-    for i in string:        
-        if i == i.upper():
-             string_count.append(i)
-
-    return len(string_count)        
+    string_count=[]   #list to store the UpperCase letters in string
+    returnValue=string.isalpha() #check to see if string contains all letters and no numbers
+    if returnValue == True:
+            for i in string:   #loop through the given string     
+                if i == i.upper():
+                     string_count.append(i)
+                     return len(string_count)  #return the count of uppercase letters, None if no uppercase found 
+    else: print('Input characters should be string only')  #message if string is not all letters               
                      
-"""  
-string= input()                   
-print(bool(re.match('^[0-9]+$', string)))
+   
+
+
+        
+                     
+
+
+
+
+print(upperCase_count('654'))                  
