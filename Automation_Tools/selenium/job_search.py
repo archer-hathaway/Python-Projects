dce5d06ef5c8ccd1761cdf96d4cc059b5b867730
mike daniels <micodian83@gmail.com>
2020-06-23 11:29:14 -0500

    Update job_search.py

--- Automation_Tools/selenium/job_search.py
+++ Automation_Tools/selenium/job_search.py
@@ -1,21 +1,27 @@
-#install selenium with pip install selenium
+#install selenium with 'pip install selenium' on your terminal/cmd
 #Using chrome, install/download chromedriver for windows
 #Mac and Linux Users can download with this https://github.com/SeleniumHQ/selenium/wiki/ChromeDriver
+#Some IDE might have issues opening chrome
 #Depending on the IDE used , you might have to run the .py file from the terminal or cmd
-# By -> python filename.py 
-#When adding the PATH, most IDE would require 'r' -> raw string so as not to get unicode error
-#The path should be the absolute path where chromedriver.exe resides
+# Run 'python filename.py' on the terminal/cmd if your prefered IDE dosent work  
+#When adding the PATH, most IDE would require 'r' -> raw string so as not to get any unicode error
+#The path should be the absolute path where chromedriver.exe resides on your computer
 from selenium import webdriver
 #from selenium.webdriver.chrome.options import options
 #import time
 import time
+#Welcome page screen
+#username and password function
+#Function would return username and password
 
-
+#Please add the path where the chromedriver resides
+#PATH = r"C:\Users\fullPathonMyPC\chromedriver.exe"
 PATH = r"C:\Users\micod\Documents\GitHub\Python-Projects\Automation_Tools\selenium\chromedriver.exe"
 
+#create driver object
 driver = webdriver.Chrome(PATH)
 
-#access the webpage specified
+#access jobsearch webpage specified
 driver.get('https://www.glassdoor.com/index.htm')
 #click on the sign-in NAV to input username and password
 driver.find_element_by_xpath('//*[@id="TopNav"]/nav/div/div/div[4]/div[1]/a').click()
@@ -23,7 +29,7 @@ driver.find_element_by_xpath('//*[@id="TopNav"]/nav/div/div/div[4]/div[1]/a').cl
 time.sleep(2)
 #log-in details 
 driver.find_element_by_name('username').send_keys('username@gmail.com')
-driver.find_element_by_name('password').send_keys('*')
+driver.find_element_by_name('password').send_keys('password')
 time.sleep(2)
 driver.find_element_by_name('submit').click()
 time.sleep(2)
@@ -36,12 +42,14 @@ time.sleep(2)
 #driver.switchTo().alert().dismiss()
 jobs_List = driver.find_elements_by_xpath("//ul[@class='jlGrid hover']/li")
 
-jobs_outfile = open('./jobs.txt','w')
 
+#create text file 
+jobs_outfile = open('./jobs.txt','w')
+#loop through the list of jobs and write to file
 for job in jobs_List:
     jobs_outfile.write(job.text)
 
-
+#close file
 jobs_outfile.close()
 
 #To create an account
